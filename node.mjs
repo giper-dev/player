34432b79e724154a786cc445c75321235c859e20
nin-jin <nin-jin@users.noreply.github.com>
2026-03-17 07:56:28 +0000

    Deploying to gh-pages from @ giper-dev/player@435e5bc8765942f4964ddf2980e4365b084eda63 🚀

--- node.mjs
+++ node.mjs
@@ -5529,6 +5529,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- node.test.js
+++ node.test.js
@@ -5520,6 +5520,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
